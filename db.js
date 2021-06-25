518ec3cfba506c6ed476f8b0e11f4cfeae711d76
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-06-25 18:40:30 +0530

    database model defined

---
+++ db.js
@@ -0,0 +1,56 @@
+const Sequelize = require("sequelize");
+
+const db = new Sequelize("shopdb", "shopper", "shoppass", {
+  host: "localhost",
+  dialect: "mysql",
+  pool: {
+    min: 0,
+    max: 5,
+  },
+});
+
+const User = db.define("users", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+const Product = db.define("products", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  manufacturer: {
+    type: Sequelize.STRING,
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+    defaultValue: 0.0,
+  },
+});
+
+// syncing model to the database
+db.sync()
+  .then(() => {
+    console.log("Database has been synced");
+  })
+  .catch((err) => {
+    console.error("Error creating database");
+  });
+
+exports = module.exports = {
+  User,
+  Product,
+};
